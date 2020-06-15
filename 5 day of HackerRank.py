e085f5390d3a16be6b2209b255b23f61eec99677
Naman-12911 <66183578+Naman-12911@users.noreply.github.com>
2020-06-15 11:32:28 +0530

    5 day of HackerRank

---
+++ 5 day of HackerRank.py
@@ -0,0 +1,38 @@
+#TASK
+#Given an integer, , print its first  multiples. Each multiple  (where ) should be printed on a new line in the form: n x i = result.
+
+#Input Format
+
+#A single integer, .
+
+#Output Format
+
+#Print  lines of output; each line  (where ) contains the  of  in the form:
+#n x i = result.
+
+#Sample Input
+
+#2
+
+#Sample Output
+
+#2 x 1 = 2
+#2 x 2 = 4
+#2 x 3 = 6
+#2 x 4 = 8
+#2 x 5 = 10
+#2 x 6 = 12
+#2 x 7 = 14
+#2 x 8 = 16
+#2 x 9 = 18
+#2 x 10 = 20
+import maths
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1 , 11):
+        product = n * i 
+        print("{} x {} = {}".format(n, i, product))
